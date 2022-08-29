db442f942a09db3ee2b65cc935285fad56b43f4d
Nikita <nik.nesterenko1999@bk.ru>
2022-08-29 14:34:04 +0300

    Задача повышенной сложности

---
+++ OOP/src/main/java/secure/SslSecure.java
@@ -0,0 +1,15 @@
+package secure;
+
+import java.util.Arrays;
+
+public class SslSecure {
+
+    private final String DOMAIN = ".com";
+    private final int DOMAIN_LENGTH = DOMAIN.length() + 2;
+    private final String PROTOCOL = "http";
+
+    public String[] urlWebSites(String[] urls) {
+
+        return new String[urls.length];
+    }
+}
